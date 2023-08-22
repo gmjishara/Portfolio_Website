950cb9ba7f177af8bc1379ef69b1cf3ac40b4fee
gmjishara <janith971231@gmail.com>
2023-08-22 12:02:51 +0530

    set animation text

--- src/Pages/HomeImageSection/HomeImage.js
+++ src/Pages/HomeImageSection/HomeImage.js
@@ -1,16 +1,33 @@
-import { Box, Button } from '@mui/material'
-import React from 'react';
+import { Box, Button } from "@mui/material";
+import React from "react";
 import "./style.css";
-import zIndex from '@mui/material/styles/zIndex';
+import zIndex from "@mui/material/styles/zIndex";
+import { TypeAnimation } from "react-type-animation";
 
 export default function HomeImage() {
-   
   return (
     <Box className="homeImage">
-        <h3>Welcome</h3>
-        <h1>I'm a Fullstack Devaloper</h1>
-        <p className='homeText'>based in Colombo, Sri Lanka.</p>
-        <button className='btn'>Hire Me</button>
+      <h3>Welcome</h3>
+      <TypeAnimation
+        sequence={[
+          "I'm Janith Ishara.",
+          1000,
+          "I'm a React Developer.",
+          1000,
+          "I'm a Frontend Developer.",
+          1000,
+          "I'm a Backend Developer.",
+          1000,
+          "I'm a Fullstack Devaloper.",
+          1000,
+        ]}
+        wrapper="span"
+        speed={10}
+        repeat={Infinity}
+        className="animateText"
+      />
+      <p className="homeText">based in Colombo, Sri Lanka.</p>
+      <button className="btn">Hire Me</button>
     </Box>
-  )
+  );
 }
