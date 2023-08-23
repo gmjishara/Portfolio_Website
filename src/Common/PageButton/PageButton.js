c791d2fc5361ba0fb7f74bdaa56b60f4fc389d65
gmjishara <janith971231@gmail.com>
2023-08-23 14:18:34 +0530

    set cv download button in skill section

--- src/Common/PageButton/PageButton.js
+++ src/Common/PageButton/PageButton.js
@@ -1,6 +1,25 @@
 import React from "react";
 import "./style.css";
+import { makeStyles } from "@mui/material";
 
 export default function PageButton({ text }) {
+  // const btnStyle = makeStyles((theme) => ({
+  //   button: {
+  //     width: "145px",
+  //     height: "48px",
+  //     marginTop: "10px",
+  //     color: `${color}`,
+  //     fontSize: "16px",
+  //     fontWeight: "500",
+  //     borderRadius: "30px",
+  //     border: `2px solid ${color}`,
+  //     background: "transparent",
+  //     cursor: "pointer",
+  //     '&hover': {
+  //       backgroundColor:`${color}`,
+  //     }
+  //   },
+
+  // }));
   return <button className="btn">{text}</button>;
 }
--- src/Components/ResumeSection/Skills/Skills.js
+++ src/Components/ResumeSection/Skills/Skills.js
@@ -2,30 +2,43 @@ import { Grid } from "@mui/material";
 import React from "react";
 import "./style.css";
 import SkillBar from "../../../Common/SkillBar/SkillBar";
+// import DownloadIcon from '@mui/icons-material/Download';
 
 export default function Skills() {
   return (
-    <div style={{marginTop:"40px"}}>
+    <div style={{ marginTop: "40px" }}>
       <h2 className="educationHeading">My Skills</h2>
       <Grid container spacing={6}>
         <Grid item sm={6}>
           <h3 className="techs">Front-end Technologies</h3>
-          <SkillBar language="HTML/CSS" percentage="85"/>
-          <SkillBar language="JavaScript" percentage="70"/>
-          <SkillBar language="React Js" percentage="75"/>
-          <SkillBar language="Redux" percentage="65"/>
-          <SkillBar language="Bootstrap" percentage="65"/>
-          <SkillBar language="Material UI" percentage="75"/>
+          <SkillBar language="HTML/CSS" percentage="85" />
+          <SkillBar language="JavaScript" percentage="70" />
+          <SkillBar language="React Js" percentage="75" />
+          <SkillBar language="Redux" percentage="65" />
+          <SkillBar language="Bootstrap" percentage="65" />
+          <SkillBar language="Material UI" percentage="75" />
         </Grid>
 
         <Grid item sm={6}>
           <h3 className="techs">Back-end Technologies</h3>
-          <SkillBar language="Java" percentage="80"/>
-          <SkillBar language="Python" percentage="65"/>
-          <SkillBar language="Laravel" percentage="65"/>
-          <SkillBar language="Spring boot" percentage="50"/>
-          <SkillBar language="MySQL" percentage="75"/>
-          <SkillBar language="Rest API" percentage="70"/>
+          <SkillBar language="Java" percentage="80" />
+          <SkillBar language="Python" percentage="65" />
+          <SkillBar language="Laravel" percentage="65" />
+          <SkillBar language="Spring boot" percentage="50" />
+          <SkillBar language="MySQL" percentage="75" />
+          <SkillBar language="Rest API" percentage="70" />
+        </Grid>
+
+        <Grid
+          item
+          sm={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "-20px",
+          }}
+        >
+          <button className="btn2">Download CV</button>;
         </Grid>
       </Grid>
     </div>
