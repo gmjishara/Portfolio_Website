8af14e5cfea6fde2af371c2468ec27e3f18c153f
gmjishara <janith971231@gmail.com>
2023-09-11 23:21:47 +0530

    created text boxes in contact area

---
+++ src/Common/TextArea/TextArea.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+import { styled } from '@mui/system';
+
+export default function TextArea({placeholder}) {
+  const blue = {
+    100: '#DAECFF',
+    200: '#b6daff',
+    400: '#3399FF',
+    500: '#007FFF',
+    600: '#0072E5',
+    900: '#003A75',
+  };
+
+  const grey = {
+    50: '#f6f8fa',
+    100: '#eaeef2',
+    200: '#d0d7de',
+    300: '#afb8c1',
+    400: '#8c959f',
+    500: '#6e7781',
+    600: '#57606a',
+    700: '#424a53',
+    800: '#32383f',
+    900: '#24292f',
+  };
+
+  const StyledTextarea = styled(TextareaAutosize)(
+    ({ theme }) => `
+    width: 100%;
+    font-family: 'Poppins',sans-serif;
+    min-height:120px;
+    font-size: 1.05rem;
+    opacity: 0.7;
+    line-height: 1.5;
+    padding: 12px;
+    border-radius: 10px 10px 0 10px;
+    color: ${grey[900]};
+    background: ${'#ffffff'};
+    border: 1px solid ${grey[300] };
+    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
+
+    &:hover {
+      border-color: ${grey[900]};
+    }
+
+    &:focus {
+      border-color: ${blue[100]};
+      box-shadow: 0 0 0 2px ${blue[500]};
+    }
+
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `,
+  );
+
+  return (
+    <StyledTextarea
+      maxRows={10}
+      placeholder={placeholder}
+    />
+  );
+}
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,41 +1,101 @@
 import React from "react";
 import SectionLayout from "../../Common/SectionLayout/SectionLayout";
-import { Box, Grid, Typography } from "@mui/material";
+import { Grid, TextField, Typography } from "@mui/material";
+import TextArea from "../../Common/TextArea/TextArea";
 
 export default function Contact() {
+  const placeholderStyles = {
+    fontWeight: "500",
+    fontFamily: "'Poppins',sans-serif",
+  };
   return (
     <div>
       <SectionLayout heading="CONTACT" topic="Get in Touch" bgColor="#F8F9FA">
-        <Grid container spacing={3}>
-          <Grid container spacing={2} sm={4} >
-            <Grid item sm={12}>
-              <Typography
-                style={{ fontSize: "19px", fontWeight: "600", opacity: "0.8" }}
-              >
-                ADDRESS
-              </Typography>
-            </Grid>
-            <Grid item sm={12}>
-              <Typography>Nethmi, Giragahahena,</Typography>
-              <Typography>South Kanumuldeniya,</Typography>
-              <Typography>Walasmulla,</Typography>
-              <Typography>Sri Lanka</Typography>
-            </Grid>
-            <Grid item sm={12}>
-              <Typography>+94 703 446 593,</Typography>
-              <Typography>janithishara971231@gmail.com</Typography>
-            </Grid>
-            <Grid item sm={12}>
-              <Typography
-                style={{ fontSize: "19px", fontWeight: "550", opacity: "0.8" }}
-              >
-                FOLLOW ME
-              </Typography>
+        <Grid container spacing={8}>
+          <Grid item sm={3}>
+            <Grid container spacing={2}>
+              <Grid item sm={12}>
+                <Typography
+                  style={{
+                    fontSize: "19px",
+                    fontWeight: "600",
+                    opacity: "0.8",
+                    fontFamily: "'Poppins',sans-serif",
+                  }}
+                >
+                  ADDRESS
+                </Typography>
+              </Grid>
+              <Grid item sm={12}>
+                <Typography style={{ fontFamily: "'Poppins',sans-serif" }}>
+                  Nethmi, Giragahahena,
+                </Typography>
+                <Typography style={{ fontFamily: "'Poppins',sans-serif" }}>
+                  South Kanumuldeniya,
+                </Typography>
+                <Typography style={{ fontFamily: "'Poppins',sans-serif" }}>
+                  Walasmulla,
+                </Typography>
+                <Typography style={{ fontFamily: "'Poppins',sans-serif" }}>
+                  Sri Lanka
+                </Typography>
+              </Grid>
+              <Grid item sm={12}>
+                <Typography style={{ fontFamily: "'Poppins',sans-serif" }}>
+                  +94 703 446 593,
+                </Typography>
+                <Typography style={{ fontFamily: "'Poppins',sans-serif" }}>
+                  janithishara971231@gmail.com
+                </Typography>
+              </Grid>
+              <Grid item sm={12}>
+                <Typography
+                  style={{
+                    fontSize: "19px",
+                    fontWeight: "550",
+                    opacity: "0.8",
+                    fontFamily: "'Poppins',sans-serif",
+                  }}
+                >
+                  FOLLOW ME
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
 
-          <Grid container spacing={3} sm={8}>
-            <Typography style={{ fontSize: "19px", fontWeight: "600", opacity: "0.8" }}>SEND US A NOTE</Typography>
+          <Grid item sm={9}>
+            <Grid container spacing={3}>
+              <Grid item sm={12}>
+                <Typography
+                  style={{
+                    fontSize: "19px",
+                    fontWeight: "600",
+                    opacity: "0.8",
+                  }}
+                >
+                  SEND US A NOTE
+                </Typography>
+              </Grid>
+              <Grid item sm={6}>
+                <TextField
+                  fullWidth
+                  placeholder="Name"
+                  style={{ backgroundColor: "#FFFFFF" }}
+                  inputProps={{ style: placeholderStyles }}
+                />
+              </Grid>
+              <Grid item sm={6}>
+                <TextField
+                  fullWidth
+                  placeholder="Email"
+                  style={{ backgroundColor: "#FFFFFF" }}
+                  inputProps={{ style: placeholderStyles }}
+                />
+              </Grid>
+              <Grid item sm={12}>
+                <TextArea placeholder="Tell us more about your needs........"/>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </SectionLayout>
