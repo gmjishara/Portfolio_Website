cc3f9d5d326018a2a5fb19c97a8064e4f0d67646
gmjishara <janith971231@gmail.com>
2023-09-13 20:15:55 +0530

    created a carousel

--- src/Pages/Testimonial/Testimonial.js
+++ src/Pages/Testimonial/Testimonial.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import SectionLayout from '../../Common/SectionLayout/SectionLayout'
+import { Carousel } from 'react-responsive-carousel'
+import ReactElasticCarousel from 'react-elastic-carousel'
+import python from "../../images/python.jpg"
+import java from "../../images/java.png"
+import travel from "../../images/travel.jpg"
 
 export default function Testimonial() {
+  const breakPoints = [
+    {width:500, itemsToShow:2},
+    {width:768, itemsToShow:2},
+    {width:1200, itemsToShow:2},
+    {width:1500, itemsToShow:2}
+  ]
+  
   return (
     <div>
         <SectionLayout heading="TESTIMONIAL" topic="Professionals Speak" bgColor="#ffffff">
-            
+        <ReactElasticCarousel breakPoints={breakPoints}>
+          <div>
+            <img src={python} height="300px" alt='python'/>
+          </div>
+          <div>
+          <img src={java} height="300px" alt='java'/>
+          </div>
+          <div>
+          <img src={travel} height="300px" alt='tarvel'/>
+          </div>
+
+        </ReactElasticCarousel>
         </SectionLayout>
     </div>
   )
