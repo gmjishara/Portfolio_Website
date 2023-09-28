6b4e7d3aaad21233aae3797d61fe83d3f23e4538
gmjishara <janith971231@gmail.com>
2023-09-29 02:01:41 +0530

    format changed

--- src/Common/LinkPopover/LinkPopover.js
+++ src/Common/LinkPopover/LinkPopover.js
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import Popover from '@mui/material/Popover';
-import Typography from '@mui/material/Typography';
-import { useState } from 'react';
+import * as React from "react";
+import Popover from "@mui/material/Popover";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
-export default function LinkPopover({children, text}) {
+export default function LinkPopover({ children, text }) {
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handlePopoverOpen = (event) => {
@@ -19,7 +19,7 @@ export default function LinkPopover({children, text}) {
   return (
     <div>
       <Typography
-        aria-owns={open ? 'mouse-over-popover' : undefined}
+        aria-owns={open ? "mouse-over-popover" : undefined}
         aria-haspopup="true"
         onMouseEnter={handlePopoverOpen}
         onMouseLeave={handlePopoverClose}
@@ -29,23 +29,23 @@ export default function LinkPopover({children, text}) {
       <Popover
         id="mouse-over-popover"
         sx={{
-          pointerEvents: 'none',
+          pointerEvents: "none",
         }}
         open={open}
         anchorEl={anchorEl}
         anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
+          vertical: "top",
+          horizontal: "left",
         }}
         transformOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
+          vertical: "bottom",
+          horizontal: "left",
         }}
         anchorPosition="100px"
         onClose={handlePopoverClose}
         disableRestoreFocus
       >
-        <Typography sx={{ p: 0.6, fontSize:10 }}>{text}</Typography>
+        <Typography sx={{ p: 0.6, fontSize: 10 }}>{text}</Typography>
       </Popover>
     </div>
   );
